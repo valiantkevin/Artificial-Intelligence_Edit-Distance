2a3ba3e27c4a2762d98680827b4c0c02791604d0
Kevin Valiant <kevin.valiant@ti.ukdw.ac.id>
2017-05-03 14:35:11 +0700

    added find minimum function and find cell value
    find cell value is not correct

--- editdistance.cpp
+++ editdistance.cpp
@@ -4,6 +4,7 @@
 using  namespace std;
 
 int checkDistance(string firstWord, string secondWord);
+int minimum(int a,int b,int c);
 
 int main ()
 {
@@ -13,20 +14,39 @@ int main ()
 
 int checkDistance(string firstWord, string secondWord)
 {
+  int distance=0;
   //matrix inizialization
   int matrix[firstWord.length()][secondWord.length()];
-  matriks[0][0]=0;
-  for (int i=1;i<firstWord.length())
+  matrix[0][0]=0;
+  for (int i=1;i<firstWord.length();i++)
     matrix[i][0]=i;
-  for (int i=1;i<secondWord.length())
+  for (int i=1;i<secondWord.length();i++)
     matrix[0][i]=i;
   //counting every cell value
   for (int i=1;i<firstWord.length();i++)
   {
     for (int j=1;j<secondWord.length();j++)
     {
-
+        matrix[i][j]=minimum(matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]+(firstWord[i]==secondWord[j]?0:2));
     }
   }
+  for (int i=0;i<firstWord.length();i++)
+  {
+    for (int j=0;j<secondWord.length();j++)
+    {
+      cout << matrix[i][j] << " ";
+    }
+    cout << endl;
+  }
   return distance;
+}
+
+int minimum(int a,int b,int c)
+{
+  if (a>b&&a>b)
+    return a;
+  if (b>c&&b>a)
+    return b;
+  if (c>a&&c>b)
+    return c;
 }
